70a4fbcb63f4d33fdae81818dff87880c97746b9
Sophia Young <syoung60@stuy.edu>
2024-09-27 10:58:39 -0400

    added arrayToString method

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -1,3 +1,16 @@
 public class MyArrays{
+public static void main(String[] args){
+
+}
+public static String arrayToString(int[] nums){
+  String ans = "[";
+  for(int i = 0; i < nums.length; i++)
+  {
+    ans = ans + nums[i];
+    if(i != nums.length - 1) ans = ans + ", ";
+  }
+  return ans + "]";
+}
+
 
 }
