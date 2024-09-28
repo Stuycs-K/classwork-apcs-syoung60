8c0f082330a32e1fd8d016b3d8d5248a91dfe815
Sophia Young <syoung60@stuy.edu>
2024-09-28 15:12:18 -0400

    added start of new methods and concatArray

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -12,5 +12,29 @@ public static String arrayToString(int[] nums){
   return ans + "]";
 }
 
+//Write a function that returns a new array with the same values as the parameter ary.
+//BEFORE write code: Discuss with your neighbors how do you test if the arrays match, and how do you test that they are not the same array?
+public static int[] returnCopy(int[]ary){
+
+}
+
+
+
+//Write a function that returns a new array, that contains all of the values of ary1 followed by all of the values of ary2. The order of the values should remain the same.
+
+public static int[] concatArray(int[]ary1,int[]ary2){
+  int[] ans = new int[ary1.length + ary2.length];
+  int i = 0;
+  for(; i < ary1.length; i++)
+  {
+    ans[i] = ary1[i];
+  }
+  for(i = ary1.length; i < ary1.length + ary2.length; i++)
+  {
+    ans[i + ary1.length] = ary2[i];
+  }
+  return ans;
+}
+
 
 }
