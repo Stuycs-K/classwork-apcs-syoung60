11b0f8803dff6c2a21a57589f016101e04fada9d
Sophia Young <syoung60@stuy.edu>
2024-09-28 16:01:01 -0400

    added test cases I made

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -1,5 +1,21 @@
 public class MyArrays{
 public static void main(String[] args){
+  int[] ary1 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+  int[] ary2 = {21, 32, 43, 54, 65, 76, 87, 98};
+  int[] ary3 = {258, 51, 7, 729, 63, 3, 13, 97, 186, 815, 48, 173, 130, 402, 183, 245};
+  int[] copy1 = returnCopy(ary1)
+  int[] copy1 = returnCopy(ary2)
+  int[] copy1 = returnCopy(ary3)
+
+  System.out.println("Comparing Array 1: " + arrayToString(ary1) + ", to its copy: " arrayToString(copy1));
+  System.out.println("Do the addresses match? " + (ary1 == copy1));
+
+  System.out.println("Comparing Array 2: " + arrayToString(ary2) + ", to its copy: " arrayToString(copy2));
+  System.out.println("Do the addresses match? " + (ary2 == copy3));
+
+  System.out.println("Comparing Array 3: " + arrayToString(ary3) + ", to its copy: " arrayToString(copy3));
+  System.out.println("Do the addresses match? " + (ary3 == copy3));
+
 
 }
 public static String arrayToString(int[] nums){
