9fb30cc759eb4573753a209db9c41193bfe4c2a8
Sophia Young <syoung60@stuy.edu>
2024-09-28 15:25:48 -0400

    added returnCopy method

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -13,14 +13,22 @@ public static String arrayToString(int[] nums){
 }
 
 //Write a function that returns a new array with the same values as the parameter ary.
-//BEFORE write code: Discuss with your neighbors how do you test if the arrays match, and how do you test that they are not the same array?
+//BEFORE write code: Discuss with your neighbors how do you test if the arrays match, 
+//and how do you test that they are not the same array?
 public static int[] returnCopy(int[]ary){
+  int[] ans = new int[ary.length]
+  for(int i = 0; i < ary.length; i++)
+  {
+    ans[i] = ary[i];
+  }
+  return ans;
 
 }
 
 
 
-//Write a function that returns a new array, that contains all of the values of ary1 followed by all of the values of ary2. The order of the values should remain the same.
+//Write a function that returns a new array, that contains all of the values of ary1 followed by all of the values of ary2. 
+//The order of the values should remain the same.
 
 public static int[] concatArray(int[]ary1,int[]ary2){
   int[] ans = new int[ary1.length + ary2.length];
