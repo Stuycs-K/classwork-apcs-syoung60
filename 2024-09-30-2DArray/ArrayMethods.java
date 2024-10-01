7e405746c06a833863c00df129a5a9e9caed1ef4
Sophia Young <syoung60@stuy.edu>
2024-10-01 14:22:15 -0400

    started new methods

--- 2024-09-30-2DArray/ArrayMethods.java
+++ 2024-09-30-2DArray/ArrayMethods.java
@@ -9,11 +9,11 @@ public static void main(String[] args){
   int[][] arr3 = new int[][] {{2, 8, 3, 4}, {1, 0, 5, 9, 5}, {6, 2, 9}}; //jagged
 
 //System.out.println(arr0[0].length);
-  
+
   System.out.println("Test if arrToStrings can make strings of 2D arrays");
   System.out.println("Expected for arr0:  [[89, 1, 73], [710, 35, 5, 2], [0], [46, 209]]");
   System.out.println("Observed for arr0:  " + arrToString(arr0));
-  System.out.println("Expected for arr1:  [[4, 76, 208, 14], [40, 951, 20, 480]]"); 
+  System.out.println("Expected for arr1:  [[4, 76, 208, 14], [40, 951, 20, 480]]");
   System.out.println("Observed for arr1:  " + arrToString(arr1));
   System.out.println("Expected for arr2:  [[18, 34, 19, 28], [10, 7, 46, 39], [38, 16, 3, 91], [33, 9, 47, 4]]");
   System.out.println("Observed for arr2:  " + arrToString(arr2));
@@ -60,7 +60,7 @@ public static String arrToString(int[][]ary){
 /*Return the sum of all of the values in the 2D array */
 public static int arr2DSum(int[][]nums){
   int sum = 0;
-  for(int i = 0; i < nums.length; i++) 
+  for(int i = 0; i < nums.length; i++)
   {
     for (int j = 0; j < nums[i].length; j++)
     {
@@ -82,5 +82,33 @@ public static int[][] swapRC(int[][]nums){
   }
  return ans;
 }
+
+//10-1
+//3. Modify a given 2D array of integer as follows:
+//Replace all the negative values:
+//-When the row number is the same as the column number replace
+//that negative with the value 1
+//-All other negatives replace with 0
+public static void replaceNegative(int[][] vals){
+  for(int i = 0; i < val.length; i++)
+  {
+    for(int j = 0; j < vals[i].length; j++)
+    {
+      if
+    }
+  }
+
+}
+
+//4. Make a copy of the given 2d array.
+//When testing : make sure that changing the original does NOT change the copy.
+//DO NOT use any built in methods that "copy" an array.
+//You SHOULD write a helper method for this.
+//If you don't see a good way to do that, you should stop and look at prior methods.
+public static int[][] copy(int[][] nums){
+  return null;//placeholder so it compiles
+}
+
+
 
 }
