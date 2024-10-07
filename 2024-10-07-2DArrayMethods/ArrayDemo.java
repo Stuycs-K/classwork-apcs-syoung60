aeafb903630957994ac7d5a7ed0d124867bb5594
Sophia Young <syoung60@stuy.edu>
2024-10-07 11:01:22 -0400

    put in more previously made methods

--- 2024-10-07-2DArrayMethods/ArrayDemo.java
+++ 2024-10-07-2DArrayMethods/ArrayDemo.java
@@ -63,7 +63,15 @@ public class ArrayDemo{
   /*Return the sum of all of the values in the 2D array
    *Use a nested loop instead of a helper method*/
   public static int arr2DSum(int[][]nums){
-    return 0;
+    int sum = 0;
+    for(int i = 0; i < nums.length; i++)
+    {
+      for (int j = 0; j < nums[i].length; j++)
+      {
+        sum = sum + nums[i][j];
+      }
+    }
+    return sum;
   }
 
   //3. Modify a given 2D array of integer as follows:
@@ -72,7 +80,17 @@ public class ArrayDemo{
   //that negative with the value 1
   //-All other negatives replace with 0
   public static void replaceNegative(int[][] vals){
-
+    for(int i = 0; i < vals.length; i++)
+    {
+      for(int j = 0; j < vals[i].length; j++)
+      {
+        if(vals[i][j] < 0)
+        {
+          vals[i][j] = 0;
+          if(j == i) vals[i][j] = 1;
+        }
+      }
+    }
   }
 
   //4. Make a copy of the given 2d array.
@@ -81,14 +99,36 @@ public class ArrayDemo{
   //You SHOULD write a helper method for this.
   //If you don't see a good way to do that, you should stop and look at prior methods.
   public static int[][] copy(int[][] nums){
-    return new int[1][1];
+    int[][] result = new int[nums.length][];
+    for (int i = 0; i < nums.length; i++)
+    {
+      result[i] = returnCopy(nums[i]);
+    }
+    return result;
+  }
+
+  public static int[] returnCopy(int[]ary){
+    int[] ans = new int[ary.length];
+    for(int i = 0; i < ary.length; i++)
+    {
+      ans[i] = ary[i];
+    }
+    return ans;
   }
 
   //5. Rotate an array by returning a new array with the rows and columns swapped.
   //   You may assume the array is rectangular and neither rows nor cols is 0.
   //   e.g. swapRC({{1,2,3},{4,5,6}}) returns {{1,4},{2,5},{3,6}}
   public static int[][] swapRC(int[][]nums){
-    return new int[1][1];
+    int[][] ans = new int[nums[0].length][nums.length];
+    for(int i = 0; i < nums[0].length; i++)//i is term in og array
+    {
+      for(int j = 0; j < nums.length; j++)//j is the array in the og 2d array
+      {
+        ans[i][j] = nums[j][i];
+      }
+    }
+   return ans;
   }
 
   //6. Make an HTML table by putting a table tag around the entire 2d array,
