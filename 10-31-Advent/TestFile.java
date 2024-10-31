41c566e0397fbe8d4c8bbadf560c8b6bc0481a18
Sophia Young <syoung60@stuy.edu>
2024-10-31 11:14:39 -0400

    copied code from website and talked with partner

--- 10-31-Advent/TestFile.java
+++ 10-31-Advent/TestFile.java
@@ -1,3 +1,18 @@
-public class Blah{
-  
+public class Main{
+  import java.util.Scanner;
+  Scanner sc = new Scanner("Some String (or File) Here");
+
+  public static void main(String[] args) {
+      String text = "This is a sample\nOf how to read\ntext!";
+
+      // creates a Scanner object to read throug the String
+      Scanner input = new Scanner(text);
+
+      //you don't want a for loop here unless you know the size of the data set
+
+      while(input.hasNext()){
+        System.out.println(input.next());
+      }
+      canner sc1 = new Scanner("This is a bunch of words");
+    }
 }
