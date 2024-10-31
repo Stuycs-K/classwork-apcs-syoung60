02f8d044691a0b6feba80ae270e48e71f38db2c5
Sophia Young <syoung60@stuy.edu>
2024-10-31 11:16:02 -0400

    copied sample code

--- 10-31-Advent/TestFile.java
+++ 10-31-Advent/TestFile.java
@@ -1,5 +1,7 @@
 public class Main{
   import java.util.Scanner;
+  import java.io.File;
+  import java.io.FileNotFoundException;
   Scanner sc = new Scanner("Some String (or File) Here");
 
   public static void main(String[] args) {
@@ -15,4 +17,21 @@ public class Main{
       }
       canner sc1 = new Scanner("This is a bunch of words");
     }
+    public class ReadFile {
+
+  public static void main(String[] args) {
+    //2 Opening a file requires a try/catch
+    try {
+      File file = new File("PUT_FILENAME_HERE");//1
+      Scanner input = new Scanner(file);
+      //CODE THAT SCANS THE FILE.
+      input.close();//releases the file from your program
+
+    } catch (FileNotFoundException ex) {
+      //File not found what should you do?
+      System.out.println("File not found");
+      return; //you can return from a void function just don't put a value.
+    }
+  }
+}
 }
