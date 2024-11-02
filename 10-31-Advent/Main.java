55abe3adfe7c99030480d17ee360ba1dddb44dd2
Sophia Young <syoung60@stuy.edu>
2024-11-02 09:46:29 -0400

    renamed and edited TestFile, so it's basically a new file

---
+++ 10-31-Advent/Main.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+
+class Main {
+  Scanner sc = new Scanner("Some String (or File) Here");
+    public static void main(String[] args) {
+      String text = "This is a sample\nOf how to read\ntext!";
+
+      // creates a Scanner object to read throug the String
+      Scanner input = new Scanner(text);
+
+      //you don't want a for loop here unless you know the size of the data set
+
+      while(input.hasNext()){
+        System.out.println(input.next());
+      }
+      
+      /*
+      while(input.hasNextLine()){
+        System.out.println(input.nextLine());
+        //do something with the next line of the file
+      }
+      int sum = 0;
+      while(input.hasNextInt()){
+        sum+=input.nextInt();//read all the numbers and add to sum
+      } */
+    }
+  }
+
+
+
+
+
+
+   
