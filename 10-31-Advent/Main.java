4ce3d469c621f79e771ef294d3d01722d7cdf8ae
Sophia Young <syoung60@stuy.edu>
2024-11-03 02:18:46 -0500

    edited Main and created ReadFile with code from website

--- 10-31-Advent/Main.java
+++ 10-31-Advent/Main.java
@@ -16,7 +16,20 @@ class Main {
       while(input.hasNext()){
         System.out.println(input.next());
       }
+
+      Scanner sc1 = new Scanner("This is a bunch of words");
+      while(sc1.hasNext()){
+        System.out.println(sc1.next());
+      }
       
+      Scanner sc2 = new Scanner("10.0 1.04 99.0 -3.0 4.2");
+      double sum = 0;
+      while(sc2.hasNext()){
+        sum = sum + (sc2.nextDouble());
+        System.out.println(sum);
+      }
+      System.out.println(sum);
+
       /*
       while(input.hasNextLine()){
         System.out.println(input.nextLine());
