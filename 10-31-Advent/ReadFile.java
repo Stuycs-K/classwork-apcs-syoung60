3cc9064a6cfcb906204491f5544982b37ca3d2ea
Sophia Young <syoung60@stuy.edu>
2024-11-03 17:44:33 -0500

    made it print lines with {

--- 10-31-Advent/ReadFile.java
+++ 10-31-Advent/ReadFile.java
@@ -9,11 +9,23 @@ public class ReadFile {
     try {
       File file = new File("ReadFile.java");//1
       Scanner input = new Scanner(file);
-      while(input.hasNextLine()){
+      /*while(input.hasNextLine()){
         System.out.println(input.nextLine());
         input.nextLine();
       }
+      input.close();//releases the file from your program*/
+
+      String line = "";
+      while(input.hasNextLine()){
+        line = input.nextLine();
+        if(line.indexOf('{') >= 0){
+            System.out.println(line);
+        }
+      }
       input.close();//releases the file from your program
+
+
+
 
     } catch (FileNotFoundException ex) {
       //File not found what should you do?
