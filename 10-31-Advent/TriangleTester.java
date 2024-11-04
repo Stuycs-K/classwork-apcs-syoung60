7e69eab7d091593fdc281d1f10d6b0028d60e610
Sophia Young <syoung60@stuy.edu>
2024-11-03 22:45:27 -0500

    finish part B(?)

--- 10-31-Advent/TriangleTester.java
+++ 10-31-Advent/TriangleTester.java
@@ -5,8 +5,6 @@ import java.util.Scanner;
 public class TriangleTester{
     
     public static void main(String[] args) {
-        System.out.println("1, 2, 1: " + validTriangle(1, 2, 1));
-        System.out.println("3, 2, 3: " + validTriangle(3, 2, 3));
         countTrianglesA("inputTri.txt");
         countTrianglesB("inputTri.txt");
     }
