23cfa183cb77d17dd6dd3f48f7673ddba367e6be
Sophia Young <syoung60@stuy.edu>
2024-11-08 02:29:52 -0500

    added tests and speak methods for valid variables

--- 2024-11-07-Pets/Driver.java
+++ 2024-11-07-Pets/Driver.java
@@ -1,5 +1,9 @@
 // When we overrided the speak method from Animal class to the Bird class,
 // it changed the print and added the line The Mighty in from the name variable.
+
+//The Bird new Animal didn't work because Bird is more specific and doesn't incudle all animals
+//All Birds are(is-a) Animals, but not all animals, like dogs and cats, can be considered Birds
+
 public class Driver{
   public static void main(String[] args){
     Animal a1 = new Animal("moo", 5, "cow");
@@ -7,7 +11,8 @@ public class Driver{
     Bird b1 = new Bird("cheep", 4, "cow2", 1.2, "red");
     b1.speak();
 
-   // Bird b2 = new Animal(...);
-   // Animal a2 = new Bird(...);
+   //Bird b2 = new Animal("chirp", 8, "duck");
+    Animal a2 = new Bird("quack", 7, "goose", 12.9, "white");
+    a2.speak();
   }
 }
