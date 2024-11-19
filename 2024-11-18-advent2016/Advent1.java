3b17ccdcb4e32af996fbcd81691c108d0e803e4a
Sophia Young <syoung60@stuy.edu>
2024-11-18 23:07:06 -0500

    copied method from triangle

--- 2024-11-18-advent2016/Advent1.java
+++ 2024-11-18-advent2016/Advent1.java
@@ -1,3 +1,32 @@
 public class AdventDay1{
+    public static void main(String[] args){
+        lkk;
+    }
+    public static int blocks(String filename){
+        int direction = 0;
+        int north = 0;
+        int east = 0;
+        int south = 0;
+        int west = 0;
+
+
+        /*
+        try{
+            File file = new File(filename);
+            Scanner input = new Scanner(file); 
+            while(input.hasNext()){
+                if(validTriangle(Integer.parseInt(input.next()),
+                                 Integer.parseInt(input.next()), 
+                                 Integer.parseInt(input.next()))) sum = sum + 1;
+                if(input.hasNextLine()) input.nextLine();
+            }
+            input.close();
+        }catch(FileNotFoundException ex){
+            System.out.println("File not found");
+        }
+
+        System.out.println(sum);
+        return sum;*/
+    }
   
 }
