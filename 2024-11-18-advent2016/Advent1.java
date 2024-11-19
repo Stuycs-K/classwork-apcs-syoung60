5ebc4bb042298d1f7eb04bbe48b0b2401b28e7d6
Sophia Young <syoung60@stuy.edu>
2024-11-19 11:01:55 -0500

    tried to do it

--- 2024-11-18-advent2016/Advent1.java
+++ 2024-11-18-advent2016/Advent1.java
@@ -1,13 +1,32 @@
-public class AdventDay1{
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Advent1{
     public static void main(String[] args){
-        lkk;
+      blocks("Advent1input.txt");
+
+
     }
     public static int blocks(String filename){
+        int x = 0;
+        int y = 0;
         int direction = 0;
-        int north = 0;
-        int east = 0;
-        int south = 0;
-        int west = 0;
+        try{
+          File file = new File(filename);
+          Scanner input = new Scanner(file);
+          while(input.hasNextLine()){
+            String line = input.nextLine();
+            System.out.println(line);}
+          }catch (FileNotFoundException ex) {
+            System.out.println("File not found");
+            }
+
+            System.out.println("help me");
+            return 0;
+
+
+        }
 
 
         /*
@@ -16,7 +35,7 @@ public class AdventDay1{
             Scanner input = new Scanner(file); //how to keep track of direction
             while(input.hasNext()){            // can sum up nesw and add together with vectorsin the end
                 if(validTriangle(Integer.parseInt(input.next()),
-                                 Integer.parseInt(input.next()), 
+                                 Integer.parseInt(input.next()),
                                  Integer.parseInt(input.next()))) sum = sum + 1;
                 if(input.hasNextLine()) input.nextLine();
             }
@@ -25,8 +44,23 @@ public class AdventDay1{
             System.out.println("File not found");
         }
 
+
+        public static String[] parse(String file){
+        Scanner inf = new Scanner(new File(file))
+        String line = inf.nextLine();
+        System.out.println
+        return line.split(",");
+        }catch (Exception e){
+        System.exit(1)
+        }
+        }
+        int solve(String[] data)
+        main (STring[] args){
+        String[] data = parse("input.txt")
+        }
+
         System.out.println(sum);
         return sum;*/
-    }
-  
+
+
 }
