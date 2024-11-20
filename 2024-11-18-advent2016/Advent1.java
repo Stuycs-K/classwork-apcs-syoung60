6d3ce83622b3a38d08800b49065655b558277aed
Sophia Young <syoung60@stuy.edu>
2024-11-20 05:26:54 -0500

    deleted unneeded comments

--- 2024-11-18-advent2016/Advent1.java
+++ 2024-11-18-advent2016/Advent1.java
@@ -15,9 +15,6 @@ public class Advent1{
         try{
           File file = new File(filename);
           Scanner input = new Scanner(file);
-          //String line = input.nextLine();
-          //System.out.println(line);
-          //cut out all commas
           while(input.hasNext()){
             String working = input.next();
             String dir = working.substring(0, 1);
@@ -47,40 +44,6 @@ public class Advent1{
 
 
         }
-
-
-        /*
-        try{
-            File file = new File(filename);
-            Scanner input = new Scanner(file); //how to keep track of direction
-            while(input.hasNext()){            // can sum up nesw and add together with vectorsin the end
-                if(validTriangle(Integer.parseInt(input.next()),
-                                 Integer.parseInt(input.next()),
-                                 Integer.parseInt(input.next()))) sum = sum + 1;
-                if(input.hasNextLine()) input.nextLine();
-            }
-            input.close();
-        }catch(FileNotFoundException ex){
-            System.out.println("File not found");
-        }
-
-
-        public static String[] parse(String file){
-        Scanner inf = new Scanner(new File(file))
-        String line = inf.nextLine();
-        System.out.println
-        return line.split(",");
-        }catch (Exception e){
-        System.exit(1)
-        }
-        }
-        int solve(String[] data)
-        main (STring[] args){
-        String[] data = parse("input.txt")
-        }
-
-        System.out.println(sum);
-        return sum;*/
 
 
 }
