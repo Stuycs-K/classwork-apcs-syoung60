297202cbf20f8976b99f36ae5fa7a16a0309da22
Sophia Young <syoung60@stuy.edu>
2024-11-20 11:00:43 -0500

    did stuff

--- 2024-11-18-advent2016/Advent1.java
+++ 2024-11-18-advent2016/Advent1.java
@@ -33,7 +33,7 @@ public class Advent1{
             //System.out.println(x + "," + y);
             //System.out.println(dir + "-" + dis + "----");
             }
-            
+
           }catch (FileNotFoundException ex) {
             System.out.println("File not found");
             }
@@ -41,7 +41,40 @@ public class Advent1{
             System.out.println(x + " in the x \n" + y + " in the y");
             System.out.println(Math.abs(x) + Math.abs(y));
             return Math.abs(x) + Math.abs(y);
+        }
+        public static int twice(String filename){
+          int x = 0;
+          int y = 0;
+          int direction = 0;
+          try{
+            File file = new File(filename);
+            Scanner input = new Scanner(file);
+            while(input.hasNext()){
+              String working = input.next();
+              String dir = working.substring(0, 1);
+              int dis = 0;
+              if(input.hasNext()) dis = Integer.parseInt(working.substring(1, working.length() - 1));
+              else {dis = Integer.parseInt(working.substring(1));}
+              if(dir.equals("R")) direction++;
+              if(dir.equals("L")) direction--;
+              if(direction == 4) direction = 0;
+              if(direction == -1) direction = 3;
 
+              if(direction == 0) y = y + dis;
+              if(direction == 1) x = x + dis;
+              if(direction == 2) y = y - dis;
+              if(direction == 3) x = x - dis;
+              //System.out.println(x + "," + y);
+              //System.out.println(dir + "-" + dis + "----");
+              }
+
+            }catch (FileNotFoundException ex) {
+              System.out.println("File not found");
+              }
+
+              System.out.println(x + " in the x \n" + y + " in the y");
+              System.out.println(Math.abs(x) + Math.abs(y));
+              return Math.abs(x) + Math.abs(y);
 
         }
 
