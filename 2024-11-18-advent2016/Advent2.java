d6b8ccacb78baaba1db1d90dbc5d4f52c745a681
Sophia Young <syoung60@stuy.edu>
2024-11-24 17:17:02 -0500

    finished part 1

--- 2024-11-18-advent2016/Advent2.java
+++ 2024-11-18-advent2016/Advent2.java
@@ -1,9 +1,12 @@
-public class AdventDay2{
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+public class Advent2{
     public static void main(String[] args){
-        lkk;
+        System.out.println(bathroom("Advent2input.txt"));
     }
     public static int bathroom(String filename){
-        String ans = "5";
+        int ans = 5;
         int num = 5;
         try{
           File file = new File(filename);
@@ -13,12 +16,83 @@ public class AdventDay2{
             String[] lineArr = line.split("");
             for(int i = 0; i < lineArr.length; i++){
                 if(lineArr[i].equals("U")){
-                    if(!=(num == 1||num == 2||num == 3)){
-                        num = num - 3;
+                    if(num == 1||num == 2||num == 3){
+                        num = num;
                     } 
+                    else{num = num - 3;}
                 }
-                //repeat if statement for each d, l, r, with exceptions
+                if(lineArr[i].equals("D")){
+                    if(num == 7||num == 8||num == 9){
+                        num = num;
+                    } 
+                    else{num = num + 3;}
+                }
+                if(lineArr[i].equals("L")){
+                    if(num == 1||num == 4||num == 7){
+                        num = num;
+                    }
+                    else{num = num - 1;} 
+                }
+                if(lineArr[i].equals("R")){
+                    if(num == 3||num == 6||num == 9){
+                        num = num;
+                    } 
+                    else{num = num + 1;}
+                }
+            }
+            ans = ans * 10 + num;
+          }
+        }catch(FileNotFoundException ex) {
+              System.out.println("File not found");
+              }
+        return ans;
+    }
 
+    public static int bathTwo(String filename){
+        String ans = "";
+        int num = 5;
+        String num2 = "";
+        try{
+          File file = new File(filename);
+          Scanner input = new Scanner(file);
+          while(input.hasNext()){
+            String line = input.next();
+            String[] lineArr = line.split("");
+            for(int i = 0; i < lineArr.length; i++){
+                if(lineArr[i].equals("U")){
+                    if(num == 1||num == 2||num == 3){
+                        num = num;
+                    } 
+                    else{num = num - 3;}
+                }
+                if(lineArr[i].equals("D")){
+                    if(num == 7||num == 8||num == 9){
+                        num = num;
+                    } 
+                    else{num = num + 3;}
+                }
+                if(lineArr[i].equals("L")){
+                    if(num == 1||num == 4||num == 7){
+                        num = num;
+                    }
+                    else{num = num - 1;} 
+                }
+                if(lineArr[i].equals("R")){
+                    if(num == 3||num == 6||num == 9){
+                        num = num;
+                    } 
+                    else{num = num + 1;}
+                }
+            }
+            ans = ans + num2;
+          }
+        }catch(FileNotFoundException ex) {
+              System.out.println("File not found");
+              }
+        return ans;
+    }
+
+                
            
 
 
@@ -46,7 +120,7 @@ public class AdventDay2{
 
         System.out.println(sum);
         return sum;*/
-    }
+    
   
 }
 
