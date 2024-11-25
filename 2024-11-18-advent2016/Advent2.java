b01561fb6bfbbcd8623cd07b202a546bce83f6aa
Sophia Young <syoung60@stuy.edu>
2024-11-25 01:16:27 -0500

    fixed error (three if statements made num cumulative over one move)

--- 2024-11-18-advent2016/Advent2.java
+++ 2024-11-18-advent2016/Advent2.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 public class Advent2{
     public static void main(String[] args){
         System.out.println(bathroom("Advent2input.txt"));
-        System.out.println("part 2");
         System.out.println(bathTwo("Advent2input.txt"));
     }
     public static int bathroom(String filename){
@@ -63,13 +62,13 @@ public class Advent2{
             for(int i = 0; i < lineArr.length; i++){
                 if(lineArr[i].equals("U")){
                     if(num == 5||num == 2||num == 1||num == 4||num == 9) num = num;
-                    if(num == 6||num == 7||num == 8||num == 10||num == 11||num == 12) num = num - 4;
-                    if(num == 13 || num == 3) num = num - 2;
+                    else if(num == 6||num == 7||num == 8||num == 10||num == 11||num == 12) num = num - 4;
+                    else{num = num - 2;}//(num == 13 || num == 3) 
                 }
                 if(lineArr[i].equals("D")){
                     if(num == 5||num == 10||num == 13||num == 12||num == 9) num = num; 
-                    if(num == 6||num == 7||num == 8||num == 2||num == 3||num == 4) num = num + 4;
-                    if(num == 1 || num == 11) num = num + 2;
+                    else if(num == 6||num == 7||num == 8||num == 2||num == 3||num == 4) num = num + 4;
+                    else{num = num + 2;}//(num == 1 || num == 11)
                 }
                 if(lineArr[i].equals("L")){
                     if(num == 1||num == 2||num == 5||num == 10||num == 13) num = num;
@@ -92,36 +91,6 @@ public class Advent2{
               }
         return ans;
     }
-
-                
-           
-
-
-        /*
-        make string by adding input and space? 
-        line.split("") makes a String array where the string is cut by the given string
-        this would reurn each character ""
-        in a grid, up is -3, down is +3, r and l are -1 and +1 except for edge nummbers
-        try{
-            File file = new File(filename);
-            Scanner input = new Scanner(file); 
-            while(input.hasNextLine()){
-                while(input.hasNext()){
-                String line = input.next();
-                if(next().equals.(""))} //directions are characters, but not separated tokens
-                //and turn line to string and evaluate each char
-                
-                ) sum = sum + 1;
-                if(input.hasNextLine()) input.nextLine();
-            }
-            input.close();
-        }catch(FileNotFoundException ex){
-            System.out.println("File not found");
-        }
-
-        System.out.println(sum);
-        return sum;*/
-    
   
 }
 
