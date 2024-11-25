754fe6c0b7b8a8bd224cd21907796c2f96b14fd2
Sophia Young <syoung60@stuy.edu>
2024-11-25 00:25:04 -0500

    worked on day 6

--- 2024-11-18-advent2016/Advent6.java
+++ 2024-11-18-advent2016/Advent6.java
@@ -2,4 +2,19 @@ public class Adevent6{
     public static main(String[] args){
         m
     }
+
+    public static int repeat(String filename){
+        //count char in firt line and make an array with the length
+        //or make a double array
+
+        String ans = "5";
+        int num = 5;
+        try{
+          File file = new File(filename);
+          Scanner input = new Scanner(file);
+          if(input.hasNext()){
+            String line = input.next();
+            String[][] = [line.length()][26];
+            String[] lineArr = line.split("");}
+          while(input.hasNext()){
 }
