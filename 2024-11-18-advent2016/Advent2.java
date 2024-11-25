78312a032163a225825eae1477481b224495b04f
Sophia Young <syoung60@stuy.edu>
2024-11-25 00:24:35 -0500

    supposedly finished part 2(answer is still incorrect)

--- 2024-11-18-advent2016/Advent2.java
+++ 2024-11-18-advent2016/Advent2.java
@@ -4,9 +4,11 @@ import java.util.Scanner;
 public class Advent2{
     public static void main(String[] args){
         System.out.println(bathroom("Advent2input.txt"));
+        System.out.println("part 2");
+        System.out.println(bathTwo("Advent2input.txt"));
     }
     public static int bathroom(String filename){
-        int ans = 5;
+        int ans = 0;
         int num = 5;
         try{
           File file = new File(filename);
@@ -48,7 +50,7 @@ public class Advent2{
         return ans;
     }
 
-    public static int bathTwo(String filename){
+    public static String bathTwo(String filename){
         String ans = "";
         int num = 5;
         String num2 = "";
@@ -60,30 +62,29 @@ public class Advent2{
             String[] lineArr = line.split("");
             for(int i = 0; i < lineArr.length; i++){
                 if(lineArr[i].equals("U")){
-                    if(num == 1||num == 2||num == 3){
-                        num = num;
-                    } 
-                    else{num = num - 3;}
+                    if(num == 5||num == 2||num == 1||num == 4||num == 9) num = num;
+                    if(num == 6||num == 7||num == 8||num == 10||num == 11||num == 12) num = num - 4;
+                    if(num == 13 || num == 3) num = num - 2;
                 }
                 if(lineArr[i].equals("D")){
-                    if(num == 7||num == 8||num == 9){
-                        num = num;
-                    } 
-                    else{num = num + 3;}
+                    if(num == 5||num == 10||num == 13||num == 12||num == 9) num = num; 
+                    if(num == 6||num == 7||num == 8||num == 2||num == 3||num == 4) num = num + 4;
+                    if(num == 1 || num == 11) num = num + 2;
                 }
                 if(lineArr[i].equals("L")){
-                    if(num == 1||num == 4||num == 7){
-                        num = num;
-                    }
-                    else{num = num - 1;} 
+                    if(num == 1||num == 2||num == 5||num == 10||num == 13) num = num;
+                    else{num = num - 1;}
                 }
                 if(lineArr[i].equals("R")){
-                    if(num == 3||num == 6||num == 9){
-                        num = num;
-                    } 
+                    if(num == 1||num == 4||num == 9||num == 12||num == 13) num = num;
                     else{num = num + 1;}
                 }
             }
+            if(num < 10) num2 = "" + num;
+            if(num == 10) num2 = "A";
+            if(num == 11) num2 = "B";
+            if(num == 12) num2 = "C";
+            if(num == 13) num2 = "D";
             ans = ans + num2;
           }
         }catch(FileNotFoundException ex) {
