0f7cf6dd78c61a16ff277e690164a856d2dc4047
Sophia Young <syoung60@stuy.edu>
2024-11-27 02:52:21 -0500

    worked on day 6

--- 2024-11-18-advent2016/Advent6.java
+++ 2024-11-18-advent2016/Advent6.java
@@ -1,20 +1,34 @@
-public class Adevent6{
-    public static main(String[] args){
-        m
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+public class Advent6{
+    public static void main(String[] args){
+        repeat("Advent6input.txt");
+        
     }
 
     public static int repeat(String filename){
         //count char in firt line and make an array with the length
         //or make a double array
-
-        String ans = "5";
-        int num = 5;
         try{
           File file = new File(filename);
           Scanner input = new Scanner(file);
-          if(input.hasNext()){
+          int[][] arr = new int[8][26];
+          int j = 0;
+          //while(input.hasNextLine()){
             String line = input.next();
-            String[][] = [line.length()][26];
-            String[] lineArr = line.split("");}
-          while(input.hasNext()){
+            String[] lineArr = line.split("");
+            char c = lineArr[0];
+            int g = c;
+            System.out.println(g);
+            
+            
+          
+        }catch(FileNotFoundException ex) {
+              System.out.println("File not found");
+              }
+        return 0;
+    }
+
+
 }
