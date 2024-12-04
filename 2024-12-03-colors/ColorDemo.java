e478e61aa9ec082b23673f4fb662d5cf97285794
Sophia Young <syoung60@stuy.edu>
2024-12-04 05:14:07 -0500

    made a blue patch

--- 2024-12-03-colors/ColorDemo.java
+++ 2024-12-03-colors/ColorDemo.java
@@ -1,13 +1,21 @@
 public class ColorDemo{
   public static void main(String[] args){
     for(int r = 0; r < 256; r+=32){
-      for(int g = 0; g <= 256; g+=32){
+      for(int g = 0; g <= 32; g+=4){
         for(int b = 0; b <= 256; b+=32){
           System.out.print("\u001b[38;2;"+r+";"+g+";"+b+";7m.");
         }
       }
-        //System.out.println();
     }
+    for(int rg = 100; rg < 180; rg+=2){
+      for(int i = 0; i < 100; i++){
+        System.out.print("\u001b[38;2;"+rg+";"+(rg + 60)+";"+250+";7m.");
+      }
+    }
+
   }
+  
 
 }
+
+
