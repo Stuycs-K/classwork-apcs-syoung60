209fbf4a859a76d415841ba68d89f38f8fcf3b8e
Sophia Young <syoung60@stuy.edu>
2024-12-06 10:59:11 -0500

    tried my best

--- 2024-12-06-Adventurer/Driver.java
+++ 2024-12-06-Adventurer/Driver.java
@@ -1,3 +1,8 @@
 public class Driver{
+  public static void main(String[] args){
+    Adventurer p1 = new Thief("Parker", 10);
+  //  System.out.println(p1.getSpecialName() + "," + p1.getSpecial());
+  }
+
 
 }
--- 2024-12-06-Adventurer/Thief.java
+++ 2024-12-06-Adventurer/Thief.java
@@ -1,5 +1,49 @@
 public class Thief extends Adventurer{
+  private int money;
+
   public Thief(String name){
     super(name);
   }
+  public Thief(String name, int hp){
+    super(name, hp);
+  }
+
+  //Abstract methods are meant to be implemented in child classes.
+
+  /*
+    all adventurers must have a custom special
+    consumable resource (mana/rage/money/witts etc)
+  */
+  //give it a short name (fewer than 13 characters)
+  public static String getSpecialName(){
+    return "money";
+  }
+  //accessor methods
+  public abstract int getSpecial(){
+    return money;
+  }
+  public abstract void setSpecial(int n){
+    money = n;
+  }
+  public abstract int getSpecialMax(){
+    return 1;
+  }
+
+  /*
+    all adventurers must have a way to attack enemies and
+    support their allys
+
+  //hurt or hinder the target adventurer
+  public abstract String attack(Adventurer other);
+
+  //heall or buff the target adventurer
+  public abstract String support(Adventurer other);
+
+  //heall or buff self
+  public abstract String support();
+
+  //hurt or hinder the target adventurer, consume some special resource
+  public abstract String specialAttack(Adventurer other);
+  */
+
 }
