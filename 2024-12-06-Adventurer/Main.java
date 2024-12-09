f667c59df63fa39323f2ff3480531a5ba49aa914
Sophia Young <syoung60@stuy.edu>
2024-12-09 10:44:23 -0500

    copied files from class

---
+++ 2024-12-06-Adventurer/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class Main {
+  public static void main(String[] args) {
+    //do this once
+    Scanner userInput = new Scanner(System.in);
+
+    //You can do the rest many times:
+
+    System.out.println("Enter username");
+    //Read one line of user input
+    String userName = userInput.nextLine();
+    //Do something with the input
+    System.out.println("Username is: " + userName);
+  }
+}
