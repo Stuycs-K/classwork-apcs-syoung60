1f55b10ad74f2db4ff867576bf38322e872da76d
Sophia Young <syoung60@stuy.edu>
2024-12-09 10:59:53 -0500

    it should promt user to type input

--- 2024-12-06-Adventurer/Game.java
+++ 2024-12-06-Adventurer/Game.java
@@ -4,11 +4,13 @@ class Game {
     Adventurer p1 = new CodeWarrior("Player", 20);
     Adventurer p2 = new CodeWarrior("Enemy", 20);
 
+    while(p1.getHP() > 0 && p2.getHP() > 0){
       System.out.println(p1.getName()+": "+ p1.getHP() + "/" +p1.getmaxHP() +"HP, "
                        + p1.getSpecial()+"/" + p1.getSpecialMax() + p1.getSpecialName());
       System.out.println(p2.getName()+": "+ p2.getHP() + "/" +p2.getmaxHP() +"HP, "
                        + p2.getSpecial()+"/" + p2.getSpecialMax() + p2.getSpecialName());
+      System.out.println("Type: (a)ttack / (sp)ecial / (su)pport / quit");
 
-  //  }
+    }
   }
 }
