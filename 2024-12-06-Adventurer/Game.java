d27ef7f166139d49d6bcc65a9f2ead5daa944a52
Sophia Young <syoung60@stuy.edu>
2024-12-10 06:00:37 -0500

    enemy moves and print results?

--- 2024-12-06-Adventurer/Game.java
+++ 2024-12-06-Adventurer/Game.java
@@ -26,12 +26,28 @@ class Game {
         p1.specialAttack(p2);
         System.out.println(p1.getName() + " specialAttacked " + p2.getName());}
       else if(move.equals("su") || move.equals("support")) p1.support();
-      else if(move.equals("quit")) return;
+      else if(move.equals("quit")){
+        System.out.println("Player has quit the game");
+        return;}
       else {System.out.println("ERROR")}
 
-    
-
+      int Enemove = (int)(Math.random() * 3);
+      if(Enemove == 0){
+        p2.attack(p1);
+        System.out.println("Enemy attack")}
+      else if(Enemove == 1){
+        p2.specialAttack(p1);
+        System.out.println("Enemy special")}
+      else{
+        p2.support();
+        System.out.println("Enemy support")}
 
     }
+    System.out.println(p1.getName()+": "+ p1.getHP() + "/" +p1.getmaxHP() +"HP, "
+                       + p1.getSpecial()+"/" + p1.getSpecialMax() + p1.getSpecialName());
+    System.out.println(p2.getName()+": "+ p2.getHP() + "/" +p2.getmaxHP() +"HP, "
+                       + p2.getSpecial()+"/" + p2.getSpecialMax() + p2.getSpecialName());
+
+
   }
 }
