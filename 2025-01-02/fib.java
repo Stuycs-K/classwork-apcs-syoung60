562e64d4e37a159dce01fa4243fe52bb2dd09327
Sophia Young <syoung60@stuy.edu>
2025-01-02 10:55:08 -0500

    wrote method

--- 2025-01-02/fib.java
+++ 2025-01-02/fib.java
@@ -1,5 +1,12 @@
-public class directory{
+public class fib{
+  public static void main(Strings[] args){
+    System.out.println(fib)
+  }
+
   public static int fib(n){
-    
+    if(n > 1){
+      return (fib(n - 1) + fib(n - 2));
+    }
+    else{return n;}
   }
 }
