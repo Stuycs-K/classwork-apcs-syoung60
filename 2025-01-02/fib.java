218bb0f186c6b47cf663533c79175279078d172e
Sophia Young <syoung60@stuy.edu>
2025-01-02 10:58:05 -0500

    code debugged

--- 2025-01-02/fib.java
+++ 2025-01-02/fib.java
@@ -1,9 +1,17 @@
 public class fib{
-  public static void main(Strings[] args){
-    System.out.println(fib)
+  public static void main(String[] args){
+    System.out.println(fib(0));
+    System.out.println(fib(1));
+    System.out.println(fib(2));
+    System.out.println(fib(3));
+    System.out.println(fib(4));
+    System.out.println(fib(5));
+    System.out.println(fib(6));
+    System.out.println(fib(7));
+    System.out.println(fib(8));
   }
 
-  public static int fib(n){
+  public static int fib(int n){
     if(n > 1){
       return (fib(n - 1) + fib(n - 2));
     }
