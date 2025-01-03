4109096d4094eeb65683c670ebcd81a4c44ea97d
Sophia Young <syoung60@stuy.edu>
2025-01-03 11:01:40 -0500

    started words

---
+++ 2025-01-02/words.java
@@ -0,0 +1,15 @@
+public class words{
+  public static void main(String[] args){
+    System.out.println(makeWords())
+
+  }
+  public static void makeWords(int remainingLetters, String result, String alphabet){
+    if(remainingLetters == 0){
+      System.out.println(result)
+    }
+    else{
+      
+    }
+
+  }
+}
