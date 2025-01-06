5bb3e9d5afdcd1aeb013bebbeb49f0157db24e6f
Sophia Young <syoung60@stuy.edu>
2025-01-06 01:42:19 -0500

    simplified

--- 2025-01-02/words.java
+++ 2025-01-02/words.java
@@ -1,18 +1,15 @@
 public class words{
   public static void main(String[] args){
-    makeWords(2, "", "abc"); 
+    makeWords(2, "", "abcdefghijklmnopqrstuvwxyz"); 
 
   }
   public static void makeWords(int remainingLetters, String result, String alphabet){
-    System.out.println("*********" + remainingLetters + " " + result + " " + alphabet);
     if(remainingLetters == 0){
       System.out.println(result);
     }
     else{
       for(int i = 0; i < alphabet.length(); i++){
-        String thing = result + alphabet.substring(i, i + 1);
-        //System.out.println
-        makeWords(remainingLetters - 1, thing, alphabet);
+        makeWords(remainingLetters - 1, result + alphabet.substring(i, i + 1), alphabet);
       }
     }
   }
